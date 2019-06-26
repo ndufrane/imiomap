06073e5bd39eee948a2f9b1bd50f705b16af66f6
ndufrane <dufrane.nicolas@gmail.com>
2019-06-26 14:51:33 +0200

    proxy upgrade

--- geoviewer/views.py
+++ geoviewer/views.py
@@ -19,4 +19,7 @@ class HomePageView(TemplateView):
 @csrf_exempt
 def proxy(request):
     requestpath = request.GET.urlencode(safe='/:?')
+    print("*")
+    print(requestpath[:-1])
+    print("*")
     return proxy_view(request, requestpath)

8c1feb44155edea214413bce34e538395e47a659
ndufrane <dufrane.nicolas@gmail.com>
2019-07-10 16:55:27 +0200

    fix csv

--- geoviewer/static/urbanmap/UrbanIdentifyResultRow.js
+++ geoviewer/static/urbanmap/UrbanIdentifyResultRow.js
@@ -16,7 +16,7 @@ define(["dojo/_base/declare","spw/widgets/SpwIdentifyResultRow", "dojo/text!./te
                 if(	key.toUpperCase().indexOf("SHAPE") != 0
                     && key.toUpperCase().indexOf("SE_ANNO_CAD_DATA" ) != 0
                     && key.toUpperCase().indexOf("OBJECTID") != 0) {
-                        //this._rowValues.push(this.nvl(this._result.feature.attributes[key],""));
+                        this._rowValues.push(this.nvl(this._result.feature.attributes[key],""));
                         var td = domConstruct.create("td", {innerHTML: this.nvl(this._result.feature.attributes[key],"")}, this.domNode);
                         td.feature = this._result.feature;
                         on(td, "click", lang.hitch(this,this.onRowClick));
